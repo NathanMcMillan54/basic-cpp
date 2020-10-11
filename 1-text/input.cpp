22b9c5c76e82fd12d1faa1a862b2228ce5cd3c81
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-10-10 20:32:18 -0400

    Added README.md, added input in 1-text/, started working on math in 2-math/.

---
+++ 1-text/input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Input int" << std::endl;
+    int i;
+    std::cout << "Enter a number value for i: " << std::endl;
+    cin >> i;
+    std::cout << "Value of i is now " << i << std::endl;
+    char x[100];
+    std::cout << "Enter a character value for x: " << std::endl;
+    cin >> x;
+    std::cout << "Value if x is now " << x << std::endl;
+}
